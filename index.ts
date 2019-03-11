556501d6cfbd5314b493dae9b9581e149a4a7133
Daniil <beltyukovdanil@gmail.com>
2019-03-11 18:50:44 +0300

    (pokupaika): move to folder

--- index.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-import { startServer } from "./src";
-
-startServer(3000);
--- secrets/index.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export const jwtSecret = fs
-  .readFileSync(path.resolve(__dirname, "./private.pem"))
-  .toString();
-export const jwtSecretVerify = fs
-  .readFileSync(path.resolve(__dirname, "./public.pem"))
-  .toString();
