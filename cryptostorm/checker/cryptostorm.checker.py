c8432519f2dd9221e45f46ac644f7aac31c252b5
yor <artempavlov1@gmail.com>
2019-03-13 14:33:01 +0300

    Add check of public key on /flags page in checker's GET

--- cryptostorm/checker/cryptostorm.checker.py
+++ cryptostorm/checker/cryptostorm.checker.py
@@ -2,18 +2,26 @@
 
 import random
 import string
-import sys
-import requests
 import re
+import sys
+import base64
+from Crypto.Cipher import AES
+from Crypto.Protocol.KDF import PBKDF2
+import requests
+import hashlib
+from html.parser import HTMLParser
 
 OK, CORRUPT, MUMBLE, DOWN, CHECKER_ERROR = 101, 102, 103, 104, 110
 SERVICENAME = "cryptostorm"
 PORT = 5000
 
 methods = {0: "SHA-1", 1: "SHA-2", 2: "Keccak", 3: "PRNG", 4: "RSA", 5: "AES"}
+methods_inv = {v: k for k, v in methods.items()}
+
 
 def generate_rand(N=16):
     return ''.join(random.choice(string.ascii_lowercase) for _ in range(N))
+
 
 def generate_name():
     return f"{generate_rand(4)}-{generate_rand(4)}-{generate_rand(4)}"
@@ -91,7 +99,6 @@ def check(*args):
         if r.status_code != 200:
             close(CORRUPT, 'Status code is not 200')
 
-
         r = s.post("http://{}:{}/add".format(team_addr, PORT), {
             "name": name,
             "flag": flag,
@@ -112,7 +119,6 @@ def check(*args):
 
         if not flag in r.text:
             close(CORRUPT, "Can't decrypt flag")
-
 
         close(OK)
 
@@ -136,6 +142,8 @@ def get(*args):
 
         close(OK, "{}:{}".format(flag_identifier, private_key))
 
+        check_storage(team_addr, flag_identifier, private_key, flag)
+
     except Exception as e:
         close(CORRUPT)
 
@@ -153,8 +161,210 @@ COMMANDS = {
 }
 
 
+def parse_table(html):
+    p = HTMLTableParser()
+    p.feed(html)
+    table = p.tables[0]
+
+    # We don't need '#' and private key in data entries, so:
+    header = table.pop(0)[1:-1]
+
+    data = dict()
+
+    # This can be turned into list of lists for better performance if necessary
+
+    for row in table:
+        data_id = int(row.pop(0))
+        data[data_id] = dict(zip(header, row))
+
+    return data
+
+
+def check_public(method, public, private, flag):
+    if method == 0:
+        res = sha1_check(private, public)
+
+    elif method == 1:
+        res = sha2_check(private, public)
+
+    elif method == 2:
+        res = sha3_check(private, public)
+
+    elif method == 3:
+        res = PRNG_check(private, public)
+
+    elif method == 4:
+        global N
+        N = 0
+        global e
+        e = 0
+        global c
+        c = []
+        exec(public, globals())
+        c = list(map(int, c))
+        private = int(private)
+        res = RSA_decrypt(private, N, c, flag)
+
+    elif method == 5:
+        res = AES_decrypt(public, private, flag)
+
+    else:
+        res = False
+
+    if not res:
+        close(CORRUPT, f'Check of public key on flags page failed for method {methods[method]}')
+    else:
+        close(OK, f"{methods[method]} check successful")
+
+
+def get_private_key(password):
+    salt = b"feeling salty, huh?"
+    kdf = PBKDF2(password, salt, 64, 3)
+    key = kdf[:32]
+    return key
+
+
+def decrypt(enc, password):
+    unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+    private_key = get_private_key(password)
+    enc = base64.b64decode(enc)
+    iv = enc[:16]
+    cipher = AES.new(private_key, AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(enc[16:]))
+
+
+def AES_decrypt(enc, password, flag):
+    decrypted = bytes.decode(decrypt(enc, password))
+    return decrypted == flag
+
+
+def sha1_check(data, hash):
+    return hashlib.sha1(data.encode()).hexdigest() == hash
+
+
+def sha2_check(data, hash):
+    return hashlib.sha256(data.encode()).hexdigest() == hash
+
+
+def sha3_check(data, hash):
+    return hashlib.sha3_256(data.encode()).hexdigest() == hash
+
+
+def PRNG_check(data, key):
+    return hashlib.md5(data.encode()).hexdigest() == key
+
+
+def Decryption(d, n, c):
+    try:
+        m = [chr(pow(char, d, n)) for char in c]
+    except:
+        close(CORRUPT, f'Check of public key on flags page failed for method RSA')
+    return ''.join(m)
+
+
+def RSA_decrypt(d, n, message, flag):
+    return Decryption(d, n, message) == flag
+
+
+def check_storage(team_addr, id, private, flag: "Only required for AES and RSA" = ''):
+    try:
+        r = requests.get(f"http://{team_addr}:{PORT}/flags")
+    except:
+        close(CORRUPT, "Request for /flags failed")
+
+    html = r.text
+    try:
+        data = parse_table(html)
+    except:
+        close(CORRUPT, "Parsing table on /flags page failed")
+
+    try:
+        entry = data[id]
+    except:
+        close(CORRUPT, f"No data for id {id} found")
+
+    try:
+        method = methods_inv[entry['Метод']]
+    except:
+        close(CORRUPT, f"Unknown method for id {id}")
+    public = entry['Открытый ключ']
+
+    try:
+        check_public(method, public, private, flag)
+    except:
+        close(CORRUPT, f"Public key check failed for id {id}")
+
+
 if __name__ == '__main__':
     try:
         COMMANDS.get(sys.argv[1], error_arg)(*sys.argv[2:])
     except Exception as ex:
         close(CHECKER_ERROR, private="INTERNAL ERROR: {}".format(ex))
+
+
+# Yeah, fuck imports
+class HTMLTableParser(HTMLParser):
+    # https://github.com/schmijos/html-table-parser-python3/blob/master/html_table_parser/parser.py
+    """ This class serves as a html table parser. It is able to parse multiple
+    tables which you feed in. You can access the result per .tables field.
+    """
+
+    def __init__(
+            self,
+            decode_html_entities=False,
+            data_separator=' ',
+    ):
+
+        HTMLParser.__init__(self)
+
+        self._parse_html_entities = decode_html_entities
+        self._data_separator = data_separator
+
+        self._in_td = False
+        self._in_th = False
+        self._current_table = []
+        self._current_row = []
+        self._current_cell = []
+        self.tables = []
+
+    def handle_starttag(self, tag, attrs):
+        """ We need to remember the opening point for the content of interest.
+        The other tags (<table>, <tr>) are only handled at the closing point.
+        """
+        if tag == 'td':
+            self._in_td = True
+        if tag == 'th':
+            self._in_th = True
+
+    def handle_data(self, data):
+        """ This is where we save content to a cell """
+        if self._in_td or self._in_th:
+            self._current_cell.append(data.strip())
+
+    def handle_charref(self, name):
+        """ Handle HTML encoded characters """
+
+        if self._parse_html_entities:
+            self.handle_data(self.unescape('&#{};'.format(name)))
+
+    def handle_endtag(self, tag):
+        """ Here we exit the tags. If the closing tag is </tr>, we know that we
+        can save our currently parsed cells to the current table as a row and
+        prepare for a new row. If the closing tag is </table>, we save the
+        current table and prepare for a new one.
+        """
+        if tag == 'td':
+            self._in_td = False
+        elif tag == 'th':
+            self._in_th = False
+
+        if tag in ['td', 'th']:
+            final_cell = self._data_separator.join(self._current_cell).strip()
+            self._current_row.append(final_cell)
+            self._current_cell = []
+        elif tag == 'tr':
+            self._current_table.append(self._current_row)
+            self._current_row = []
+        elif tag == 'table':
+            self.tables.append(self._current_table)
+            self._current_table = []
